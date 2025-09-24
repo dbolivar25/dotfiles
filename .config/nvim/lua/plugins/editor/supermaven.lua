9db920fcfb218e31dac49c690d62e780b5a23366
dbolivar25 <danielbolivarflorez@gmail.com>
2025-09-24 00:42:32 -0400

    add supermaven

---
+++ .config/nvim/lua/plugins/editor/supermaven.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "supermaven-inc/supermaven-nvim",
+        config = function()
+            require("supermaven-nvim").setup({
+                disable_inline_completion = false,
+            })
+        end,
+    },
+}
