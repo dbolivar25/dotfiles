3200d6ec19aef5868dbc1913dc9fba78c7afd765
dbolivar25 <danielbolivarflorez@gmail.com>
2025-09-24 02:28:05 -0400

    add prisma back

--- .config/nvim/lua/plugins/lang/treesitter.lua
+++ .config/nvim/lua/plugins/lang/treesitter.lua
@@ -19,6 +19,7 @@ return {
                 "json",
                 "yaml",
                 "toml",
+                "prisma",
                 "query",
                 "regex",
                 "markdown",
